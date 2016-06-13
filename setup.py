3fa7ed8a9f47878a5c1610c4fdeb38a659a41f52
ericoporto <ericoporto2008@gmail.com>
2016-06-13 20:29:18 -0300

    fixed missing icon.png

--- setup.py
+++ setup.py
@@ -29,6 +29,6 @@ setup(
     package_dir = {"": "src"},
     scripts = ["fgmk"],
     package_data = {
-        'fgmk': ['img/*.png','coreimg/*.png','actions/*.json','*.json','basegame.tar.gz']
+        'fgmk': ['img/*.png','coreimg/*.png','actions/*.json','*.json','basegame.tar.gz', 'icon.png']
     }
 )
