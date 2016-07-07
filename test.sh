9a5305af199abe68ca346238c86031f61b74dd39
ericoporto <ericoporto2008@gmail.com>
2016-07-06 22:23:59 -0300

    adding explanation of error with pyqt5 and py.test

--- test.sh
+++ test.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+#
+# why a bash script is doing my tests you ask?
+#
+# I want to follow good integration as here:
+# http://pytest.org/latest/goodpractices.html
+#
+# But unfortunately, using like this gives me
+# error with PyQt5 as below:
+#
+# $ python3 setup.py test
+# running test
+# Searching for pyqt5
+# Reading https://pypi.python.org/simple/pyqt5/
+# No local packages or download links found for pyqt5
+# error: Could not find suitable distribution for Requirement.parse('pyqt5')
+#
+# Maybe in the future.
 
 export PYTHONPATH=src/
 py.test
