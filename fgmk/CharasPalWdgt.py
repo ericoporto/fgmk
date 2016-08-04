b3fa42a34b7ffc192922873a59addb9e5082606b
Erico Vieira Porto <ericoporto2008@gmail.com>
2016-08-03 22:07:02 -0300

    A chara now uses the undostack, so you can ctrl+z adding a chara to the map!

---
+++ fgmk/CharasPalWdgt.py
@@ -0,0 +1,115 @@
+from PyQt5 import QtGui, QtCore, QtWidgets
+from fgmk import Charas, current_project
+
+class CommandAddChara(QtWidgets.QUndoCommand):
+    """
+    Class for a single chara insert operation.
+    This class operates in the visible map
+    widget and the map (that has the jsontree), having redo (which is also the
+    do action) and undo capabilities.
+    """
+    def __init__(self, description, pCharasPalWidget,  position=(0, 0), chara=None,):
+        super().__init__(description)
+
+        self.pCharasPalWidget = pCharasPalWidget
+        self.position = position
+        if (chara == None):
+            chara = self.pCharasPalWidget.myCharaSelector.getSelected()
+        self.chara = chara
+
+    def redo(self):
+        self.pCharasPalWidget.addCharaAction(self.position,self.chara, True)
+
+    def undo(self):
+        self.pCharasPalWidget.deletePosition(self.position, True)
+
+
+class CharasPalWidget(QtWidgets.QWidget):
+    def __init__(self, mapWdgt, pMap, parent=None, charaInstance=None, **kwargs):
+        super().__init__(parent, **kwargs)
+
+        self.mapWdgt = mapWdgt
+        self.pMap = pMap
+        self.parent = parent
+
+        self.vbox = QtWidgets.QVBoxLayout(self)
+
+        self.charaslist = []
+        self.myCharaSelector = Charas.CharaSelector(self, current_project.settings)
+        self.vbox.addWidget(self.myCharaSelector)
+        self.show()
+
+    def reinit(self):
+        for charaplaced in self.charaslist:
+            charaplaced[2].stop()
+            self.mapWdgt.Grid.removeWidget(charaplaced[2])
+            charaplaced[2].deleteLater()
+
+        self.myCharaSelector.update()
+        self.charaslist = []
+
+        charalist = self.pMap.getCharaList()
+        if(charalist == [''] or not charalist):
+            return
+
+        for char in charalist:
+            self.addCharaAction((char[1], char[2]), char[0], False)
+
+    def addCharaAction(self, position=(0, 0), chara=None, onmap=True):
+        if (chara == None):
+            chara = self.myCharaSelector.getSelected()
+
+        if (chara != None):
+            scale = self.mapWdgt.myScale / 2.0
+            if(self.positionEmpty(position)):
+                item = Charas.MiniCharaTile(
+                    None, current_project.settings, chara, (0, 0), scale)
+                item.rightClicked.connect(self.autodelete)
+                self.mapWdgt.Grid.addWidget(item, position[1], position[0])
+                if(onmap):
+                    self.pMap.insertChara(position[0], position[1], chara)
+                self.charaslist.append((chara, position, item))
+
+    def autodelete(self):
+        item = self.sender()
+        for charaplaced in self.charaslist:
+            if(charaplaced[2] == item):
+                charaplaced[2].stop()
+                self.pMap.removeChara(charaplaced[1][0], charaplaced[1][1])
+                self.mapWdgt.Grid.removeWidget(charaplaced[2])
+                charaplaced[2].deleteLater()
+                break
+
+        self.charaslist.remove(charaplaced)
+
+    def getCharasList(self):
+        charaslist = []
+        for charaplaced in self.charaslist:
+            charaslist.append(charaplaced[0], charaplaced[
+                              1][0], charaplaced[1][1])
+
+        return charaslist
+
+    def deletePosition(self, position=(0, 0), onmap=False):
+        for charaplaced in self.charaslist:
+            if(charaplaced[1] == position):
+                charaplaced[2].stop()
+                if(onmap):
+                    self.pMap.removeChara(charaplaced[1][0], charaplaced[1][1])
+                self.mapWdgt.Grid.removeWidget(charaplaced[2])
+                charaplaced[2].deleteLater()
+                break
+
+        self.charaslist.remove(charaplaced)
+
+
+    def positionEmpty(self, position):
+        for charaplaced in self.charaslist:
+            if(charaplaced[1] == position):
+                return False
+
+        else:
+            return True
+
+    def getSelected(self):
+        return self.myCharaSelector.getSelected()
--- fgmk/Editor.py
+++ fgmk/Editor.py
@@ -5,7 +5,7 @@ import tarfile
 from PyQt5 import QtGui, QtCore, QtWidgets
 from fgmk import Tile, actionDialog, Editor_MainWindow_Menus, game_server, fifl, TileCharaset, Charas, gameInit, current_project
 from fgmk import  paletteWdgt, ToolsWdgt, EventsWdgt, LayerWdgt, actionsWdgt, MapExplorerWdgt, getdata, mapfile, TileSet, gameInit, configure_project
-from fgmk import help
+from fgmk import help, CharasPalWdgt
 from fgmk.flowlayout import FlowLayout as FlowLayout
 
 
@@ -170,7 +170,9 @@ class MapWidget(QtWidgets.QWidget):
             # charaplacer
             charaX = self.sender().tileX
             charaY = self.sender().tileY
-            self.parent.myCharasPalWidget.addCharaAction((charaX, charaY))
+            command = CharasPalWdgt.CommandAddChara("place chara", self.parent.myCharasPalWidget, (charaX, charaY))
+            self.parent.undoStack.push(command)
+#            self.parent.myCharasPalWidget.addCharaAction((charaX, charaY))
 
         else:
             firstClickX = None
@@ -203,92 +205,6 @@ class MapWidget(QtWidgets.QWidget):
         self.parent.undoStack.push(command)
 
 
-class CharasPalWidget(QtWidgets.QWidget):
-    def __init__(self, mapWdgt, pMap, parent=None, charaInstance=None, **kwargs):
-        super().__init__(parent, **kwargs)
-
-        self.mapWdgt = mapWdgt
-        self.pMap = pMap
-        self.parent = parent
-
-        self.vbox = QtWidgets.QVBoxLayout(self)
-
-        self.charaslist = []
-        self.myCharaSelector = Charas.CharaSelector(self, current_project.settings)
-        self.vbox.addWidget(self.myCharaSelector)
-        self.show()
-
-    def reinit(self):
-        for charaplaced in self.charaslist:
-            charaplaced[2].stop()
-            self.mapWdgt.Grid.removeWidget(charaplaced[2])
-            charaplaced[2].deleteLater()
-
-        self.myCharaSelector.update()
-        self.charaslist = []
-
-        charalist = self.pMap.getCharaList()
-        if(charalist == [''] or not charalist):
-            return
-
-        for char in charalist:
-            self.addCharaAction((char[1], char[2]), char[0], False)
-
-    def addCharaAction(self, position=(0, 0), chara=None, onmap=True):
-        if (chara == None):
-            chara = self.myCharaSelector.getSelected()
-
-        if (chara != None):
-            scale = self.mapWdgt.myScale / 2.0
-            if(self.positionEmpty(position)):
-                item = Charas.MiniCharaTile(
-                    None, current_project.settings, chara, (0, 0), scale)
-                item.rightClicked.connect(self.autodelete)
-                self.mapWdgt.Grid.addWidget(item, position[1], position[0])
-                if(onmap):
-                    self.pMap.insertChara(position[0], position[1], chara)
-                self.charaslist.append((chara, position, item))
-
-    def autodelete(self):
-        item = self.sender()
-        for charaplaced in self.charaslist:
-            if(charaplaced[2] == item):
-                charaplaced[2].stop()
-                self.pMap.removeChara(charaplaced[1][0], charaplaced[1][1])
-                self.mapWdgt.Grid.removeWidget(charaplaced[2])
-                charaplaced[2].deleteLater()
-                break
-
-        self.charaslist.remove(charaplaced)
-
-    def getCharasList(self):
-        charaslist = []
-        for charaplaced in self.charaslist:
-            charaslist.append(charaplaced[0], charaplaced[
-                              1][0], charaplaced[1][1])
-
-        return charaslist
-
-    def deletePosition(self, position=(0, 0)):
-        for charaplaced in self.charaslist:
-            if(charaplaced[1] == position):
-                charaplaced[2].stop()
-                self.mapWdgt.Grid.removeWidget(charaplaced[2])
-                charaplaced[2].deleteLater()
-                break
-
-        self.charaslist.remove(charaplaced)
-
-    def positionEmpty(self, position):
-        for charaplaced in self.charaslist:
-            if(charaplaced[1] == position):
-                return False
-
-        else:
-            return True
-
-    def getSelected(self):
-        return self.myCharaSelector.getSelected()
 
 
 class ExitFSWidget(QtWidgets.QWidget):
@@ -452,7 +368,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.viewMenu.addAction(self.paletteDockWdgt.toggleViewAction())
 
-        self.myCharasPalWidget = CharasPalWidget(
+        self.myCharasPalWidget = CharasPalWdgt.CharasPalWidget(
             self.myMapWidget, self.myMap, self)
         self.charasDockWdgt = QtWidgets.QDockWidget("Charas", self)
         self.charasDockWdgt.setObjectName("Charas")
