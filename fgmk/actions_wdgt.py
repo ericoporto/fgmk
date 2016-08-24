3b4fdd676b10770fd428bed700222c715f110fe1
Erico Vieira Porto <ericoporto2008@gmail.com>
2016-08-24 16:58:15 -0300

    separating the actions list widget used in persona to be used in other things too

--- fgmk/actions_wdgt.py
+++ fgmk/actions_wdgt.py
@@ -86,3 +86,175 @@ class ActionsWidget(QtWidgets.QDialog):
 
     def getValue(self):
         return self.returnValue
+
+
+class tinyActionsWdgt(QtWidgets.QWidget):
+    def __init__(self, parent=None, ssettings={}, ischara=False , **kwargs):
+        #super().__init__(parent, **kwargs)
+        QtWidgets.QWidget.__init__(self, parent, **kwargs)
+
+        self.ssettings = ssettings
+        self.parent = parent
+        self.ischara = ischara
+
+        self.HBox = QtWidgets.QHBoxLayout(self)
+        self.HBox.setAlignment(QtCore.Qt.AlignTop)
+
+        self.labelActionList = QtWidgets.QLabel("List of Actions:")
+        self.ActionList = QtWidgets.QListWidget(self)
+
+        VBoxActionList = QtWidgets.QVBoxLayout()
+        VBoxButtons = QtWidgets.QVBoxLayout()
+
+        self.addActionButton = QtWidgets.QPushButton("Add Action", self)
+        self.editActionButton = QtWidgets.QPushButton("Edit Action", self)
+        self.removeActionButton = QtWidgets.QPushButton("Remove Action", self)
+        self.deselectActionButton = QtWidgets.QPushButton("Deselect Actions", self)
+
+        self.checkboxes = []
+        self.checkboxes.append(QtWidgets.QCheckBox("on click", self))
+        self.checkboxes.append(QtWidgets.QCheckBox("on over", self))
+
+        self.addActionButton.clicked.connect(self.addAction)
+        self.editActionButton.clicked.connect(self.editAction)
+        self.removeActionButton.clicked.connect(self.removeAction)
+        self.deselectActionButton.clicked.connect(self.deselectAction)
+
+        self.HBox.addLayout(VBoxActionList)
+        self.HBox.addLayout(VBoxButtons)
+
+        VBoxActionList.addWidget(self.labelActionList)
+        VBoxActionList.addWidget(self.ActionList)
+
+        VBoxButtons.addWidget(self.addActionButton)
+        VBoxButtons.addWidget(self.editActionButton)
+        VBoxButtons.addWidget(self.removeActionButton)
+        VBoxButtons.addWidget(self.deselectActionButton)
+
+        self.checkboxes[0].setCheckState(QtCore.Qt.Checked)
+        self.checkboxes[1].setCheckState(QtCore.Qt.Unchecked)
+
+        for checkbox in self.checkboxes:
+            VBoxButtons.addWidget(checkbox)
+
+        self.ActionList.setDragDropMode(QtWidgets.QAbstractItemView.InternalMove)
+
+        self.ActionList.itemSelectionChanged.connect(self.enableButtonsBecauseActionList)
+
+        ActionListModel = self.ActionList.model()
+        ActionListModel.layoutChanged.connect(self.updateActionFromWidget)
+
+    def setList(self,actionToSet):
+        atype = actionToSet['type']
+        for i in range(len(atype)):
+            if(atype[i]):
+                self.checkboxes[i].setCheckState(QtCore.Qt.Checked)
+            else:
+                self.checkboxes[i].setCheckState(QtCore.Qt.Unchecked)
+
+        listToSet = actionToSet['list']
+        self.ActionList.clear()
+        for action in listToSet:
+            self.ActionList.addItem(actionItem(action))
+
+    def clear(self):
+        self.ActionList.clear()
+
+    def setAllState(self, state):
+        self.addActionButton.setEnabled(state)
+        self.removeActionButton.setEnabled(state)
+        self.ActionList.setEnabled(state)
+        self.labelActionList.setEnabled(state)
+        self.deselectActionButton.setEnabled(state)
+        self.editActionButton.setEnabled(state)
+        self.enableButtonsBecauseActionList()
+
+    def updateActionFromWidget(self):
+        i = 0
+        while i < self.ActionList.count():
+            item = self.ActionList.item(i)
+            actionToAdd = item.getAction()
+            i += 1
+
+    def editAction(self):
+        if(self.ssettings == {} ):
+            return
+
+        indexOfAction = self.ActionList.row(self.ActionList.selectedItems()[0])
+        selecteditem = self.ActionList.selectedItems()[0]
+        actionParamToEdit = selecteditem.getAction()
+
+        actionToEdit = actionParamToEdit[0]
+        paramOfEdit = actionParamToEdit[1]
+
+        paramArrayOfEdit = paramOfEdit.split(';')
+
+        newDialogFromName = getattr(action_dialog, str(actionToEdit))
+
+        self.myActionsDialog = newDialogFromName(self.ssettings["gamefolder"],self,paramArrayOfEdit,True)
+        if self.myActionsDialog.exec_() == QtWidgets.QDialog.Accepted:
+            returnActDlg = str(self.myActionsDialog.getValue())
+
+            actionToAdd = [actionToEdit,str(returnActDlg)]
+
+            self.ActionList.takeItem(indexOfAction)
+            self.ActionList.insertItem(indexOfAction,actionItem(actionToAdd))
+
+    def deselectAction(self):
+        for i in range(self.ActionList.count()):
+            item = self.ActionList.item(i)
+            self.ActionList.setItemSelected(item, False)
+
+    def addAction(self):
+        if(self.ssettings == {} ):
+            return
+
+        self.myActionsWidget = ActionsWidget(self.ssettings,self,self.ischara)
+        if self.myActionsWidget.exec_() == QtWidgets.QDialog.Accepted:
+            actionToAdd = self.myActionsWidget.getValue()
+
+            if not self.ActionList.selectedItems():
+                self.ActionList.addItem(actionItem(actionToAdd))
+            else:
+                indexOfAction = self.ActionList.row(self.ActionList.selectedItems()[0])
+                self.ActionList.insertItem(indexOfAction,actionItem(actionToAdd))
+
+    def removeAction(self):
+        if(self.ssettings == {} ):
+            return
+
+        for item in self.ActionList.selectedItems():
+            itemIndex = self.ActionList.row(item)
+            self.ActionList.takeItem(itemIndex)
+
+    def enableButtonsBecauseActionList(self):
+        enable = True
+        if (self.ActionList.currentItem() is None):
+            enable = False
+        else:
+            if (self.ActionList.currentItem().isSelected() == False):
+                enable = False
+
+        if (enable):
+            self.removeActionButton.setEnabled(True)
+            self.deselectActionButton.setEnabled(True)
+            self.editActionButton.setEnabled(True)
+        else:
+            self.removeActionButton.setEnabled(False)
+            self.editActionButton.setEnabled(False)
+            self.deselectActionButton.setEnabled(False)
+
+    def getValue(self):
+
+        allactions = []
+        for itemIndex in range(self.ActionList.count()):
+            allactions.append(self.ActionList.item(itemIndex).getAction())
+
+        onclick = self.checkboxes[0].isChecked()
+        onover = self.checkboxes[1].isChecked()
+
+        actiontype = [onclick,onover]
+
+        returnvalue = {'list':allactions, 'type':actiontype }
+
+        return returnvalue
