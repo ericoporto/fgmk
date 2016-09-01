e3940d505369c55c0b6ffb887bb84ac16b752473
Erico Vieira Porto <ericoporto2008@gmail.com>
2016-09-01 19:25:02 -0300

    commenting the game_server module

--- fgmk/game_server.py
+++ fgmk/game_server.py
@@ -2,8 +2,27 @@ import os
 import sys
 from threading import Thread
 import webbrowser
-#import BaseHTTPServer
-#import SimpleHTTPServer
+
+"""
+This module aims to serve the game so the html can be opened in Chrome without
+the json queries failing for CORS.
+
+The main function here is servePage, which will receive a directory that has the
+game, nameded urlToServe and will spawn a server to serve this page, and then
+open a browser.
+
+TODO:
+In reality it would be more ideal to spawn a server when a project is loaded and
+just open a browser pointing to the served ip and port when requested.
+
+Right now I have to have two servers, one for serving the page and the other to
+monitor if the page is closed. The js engine has a listener to request a random
+json from the port of the control server when the page is closed (or reloaded),
+and the control server (httpdAdm) will detect this and kill the main web server
+(httpdGame).
+"""
+
+# Make DEBUG=True to view log messages from servers.
 DEBUG = False
 
 try:
@@ -13,7 +32,13 @@ except ImportError:
     import BaseHTTPServer
     from  SimpleHTTPServer import SimpleHTTPRequestHandler as SimpleHTTPRequestHandler
 
+
 class NoCacheHTTPRequestHandler(SimpleHTTPRequestHandler):
+    """
+    This class makes sure that there is no cache when serving the page,
+    and that the terminal output is silent unless DEBUG is True.
+    """
+
     def end_headers(self):
         self.send_my_headers()
         SimpleHTTPRequestHandler.end_headers(self)
@@ -31,7 +56,21 @@ class NoCacheHTTPRequestHandler(SimpleHTTPRequestHandler):
         else:
             return
 
+def openBrowser(ip,port):
+    """
+    Open browser pointing to the served ip and port.
+    note the debug variable passed so the js engine creates a listener to notify
+    the adm server when the page is closed (or reloaded).
+    """
+    url = "http://" + ip + ":{0}".format(port)+"?debug=1"
+    new = 2  # 2 goes to new tab, 0 same and 1 window.
+    browserOk = webbrowser.open(url, new=new)
+
 def servePage(urlToServe):
+    """
+    Creates the server
+    """
+
     tempOrigiCurDir = os.curdir
     os.chdir(urlToServe)
     serverClass = BaseHTTPServer
@@ -42,9 +81,6 @@ def servePage(urlToServe):
     ip = '127.0.0.1'
     admIp = ip
     admPort = 8081
-
-    new = 2  # 2 goes to new tab, 0 same and 1 window.
-    url = "http://" + ip + ":{0}".format(port)+"?debug=1"
 
     handlerClass.protocol = Protocol
     try:
@@ -59,7 +95,8 @@ def servePage(urlToServe):
     if DEBUG:
         print("\n---\nServing HTTP on {0}, port {1}\n---\n".format(sa[0], sa[1]))
         print("\n---\nAdm HTTP listening on {0}, port {1}\n---\n".format(sb[0], sb[1]))
-    browserOk = webbrowser.open(url, new=new)
+
+    openBrowser(ip,port)
 
     def runGameServer():
         httpdGame.serve_forever()
