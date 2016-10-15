c60a32a40184f93460ad454917704c71b85aee5e
Erico Vieira Porto <ericoporto2008@gmail.com>
2016-10-14 23:39:54 -0300

    adding a script so that taging and uploading to pypi is easier

---
+++ dontpack/ReleaseToPypi.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+cd ..
+
+MESSAGE=$1
+VERSION=`grep __version__ fgmk/__init__.py | cut -d= -f2 | tr -d '[[:space:]]'`
+
+echo "git tag message is: $MESSAGE"
+echo "fgmk version is: $VERSION"
+
+read -p "press any key to continue " -n 1 -r
+
+
+read -p "Should we add this tag? " -n 1 -r
+echo  ''
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+
+  git tag $VERSION -m $MESSAGE
+  git push --tags origin master
+
+  python3 setup.py sdist
+  LATESTDIST=`ls -td dist/* | head -1`
+
+  echo "the distributable is: $LATESTDIST"
+
+  read -p "Should we upload to PyPI? " -n 1 -r
+  echo  ''
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+
+    twine register $LATESTDIST
+    twine upload dist/$LATESTDIST
+
+  fi
+
+fi
